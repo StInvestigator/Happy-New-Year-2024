1affdca71bc79a864327dad278dc2c35ef11bd5f
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-12-25 12:56:47 +0200

    Release state

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using Academy.Domain.Navigation;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Happy_New_Year_2024
 {
--- Presentation/TreeGame.xaml.cs
+++ Presentation/TreeGame.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using Academy.Domain.Navigation;
-using System;
-using System.Collections.Generic;
-using System.DirectoryServices.ActiveDirectory;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Happy_New_Year_2024
 {
